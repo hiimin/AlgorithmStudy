75419af1fb652b0bff250bdaf463f06c0f223170
DESKTOP-V77LQF3\LeeJeongmin <ljm245@naver.com>
2018-07-16 23:42:04 +0900

    2nd assignment #1
    recursion
    find integer pairs such that their sum is equal to k

--- 프로그래밍 과제/algorithm2/src/algorithm2/Algorithm2_1.java
+++ 프로그래밍 과제/algorithm2/src/algorithm2/Algorithm2_1.java
@@ -1,10 +1,39 @@
 package algorithm2;
 
+import java.util.Scanner;
+
 public class Algorithm2_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int N;
+		int K;
+		int data[];
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		data = new int[N];
+		for(int i = 0;i<N;i++) {
+			data[i] = sc.nextInt();
+		}
+		K = sc.nextInt();
+		sc.close();
+		
+		System.out.println(numberOfPairs(data, 0, N-1, K));
+		
+		return;
 	}
-
+	
+	public static int numberOfPairs(int data[],int start, int end, int k) {
+		if(start>=end)
+			return 0;
+		else {
+			int sum = data[start]+data[end];
+			if(sum==k) 
+				return 1+numberOfPairs(data, start+1,end-1,k);
+			else if(sum>k)
+				return numberOfPairs(data, start, end-1, k);
+			else
+				return numberOfPairs(data, start+1,end, k);
+		}
+	}
 }
